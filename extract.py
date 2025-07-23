240f76485de202dd7957d208cff3df9306a9a15f
wendelspires <wendelspires@hotmail.com>
2025-07-22 22:16:30 -0300

    Extract que foi para a AWS

--- extract.py
+++ extract.py
@@ -2,10 +2,15 @@ import base64
 import json
 from datetime import date, datetime
 import requests
+import logging
+
+# Configuração do logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info("Iniciando o processo de extração de dados.")
 
 # Imports específicos do PySpark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, current_date, date_format, regexp_replace, trim
+from pyspark.sql.functions import col, lit, current_date, date_format, regexp_replace, trim, to_date
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, LongType, DateType
 
 def generate_b3_url(day):
@@ -13,14 +18,14 @@ def generate_b3_url(day):
     Gera a URL da B3 para um dia específico.
     """
     base_url = "https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetPortfolioDay/"
-
+    
     # Objeto JSON com os parâmetros da requisição
     params = {
         "language": "pt-br",
         "pageNumber": 1,
         "pageSize": 150,
         "index": "IBOV",
-        "segment": "1",
+        "segment": "2",
         "refDate": day.strftime('%Y-%m-%d')
     }
 
@@ -34,41 +39,57 @@ def generate_b3_url(day):
 
 def extract_data_from_page_spark(url, spark):
     """
-    Extrai dados da API da B3 e os transforma usando PySpark.
+    Extrai dados da API da B3, enriquece com uma coluna de partição e salva no S3.
     """
+    logging.info(f"Extraindo dados da URL: {url}")
     res = requests.get(url)
+    if res.status_code != 200:
+        logging.error(f"Erro ao acessar a URL: Status {res.status_code} | URL: {url}")
+        # Em um pipeline real, você poderia decidir parar ou apenas logar e continuar
+        return
 
-    if res.status_code == 200:
-        data = json.loads(res.text)
-        if 'results' not in data or not data['results']:
-            print("Alerta: Dados não encontrados ou lista de 'results' vazia. Retornando DataFrame vazio.")
-            return spark.createDataFrame([], schema="data_referencia date, ticker string, nome_ativo string, tipo_ativo string, quantidade_teorica long, participacao_percentual float")
-        
-        results_schema = StructType([
-            StructField("segment", StringType(), True),
-            StructField("cod", StringType(), True),
-            StructField("asset", StringType(), True),
-            StructField("type", StringType(), True),
-            StructField("part", StringType(), True),
-            StructField("partAcum", StringType(), True),
-            StructField("theoricalQty", StringType(), True)
-        ])
+    data = json.loads(res.text)
+    if 'results' not in data or not data['results']:
+        logging.warning("Alerta: Dados não encontrados ou lista de 'results' vazia. Nenhum dado será ingerido.")
+        return
 
-        df_raw = spark.createDataFrame(data['results'], schema=results_schema)
-        header_data = data.get('header', {})
-        for key, value in header_data.items():
-            column_literal = lit(value)
-            if value is None:
-                column_literal = column_literal.cast(StringType())
+    logging.info("Criando o DataFrame Spark a partir dos dados extraídos.")
 
-            df_raw = df_raw.withColumn(f"header_{key}", column_literal)
+    # 1. Schema para os dados dos ativos
+    results_schema = StructType([
+        StructField("segment", StringType(), True),
+        StructField("cod", StringType(), True),
+        StructField("asset", StringType(), True),
+        StructField("type", StringType(), True),
+        StructField("part", StringType(), True),
+        StructField("partAcum", StringType(), True),
+        StructField("theoricalQty", StringType(), True)
+    ])
 
-        raw_data_path = "./data/raw"
-        df_raw.write.mode("overwrite").parquet(raw_data_path)
-        print(f"Dados brutos salvos com sucesso em: {raw_data_path}")
+    df = spark.createDataFrame(data['results'], schema=results_schema)
+    header_data = data.get('header', {})
 
-    else:
-        raise Exception(f"Erro ao acessar a URL: Status {res.status_code} | URL: {url}")
+    df_with_metadata = df.withColumn("header_reductor", lit(header_data.get("reductor"))) \
+                         .withColumn("header_theoricalQty", lit(header_data.get("theoricalQty")))
+
+    df_final = df_with_metadata.withColumn(
+        "dt_referencia",
+        date_format(to_date(lit(header_data.get("date")), "dd/MM/yy"), "yyyyMMdd")
+    )
+
+    # logging.info(f"Salvando dados no S3 em: {s3_base_path}")
+    # df_final.write \
+    #     .partitionBy("dt_referencia") \
+    #     .mode("overwrite") \
+    #     .parquet(s3_base_path)
+
+    # logging.info(f"Dados brutos particionados salvos com sucesso em: {s3_base_path}")
+    # print("Amostra dos dados salvos no S3:")
+    df_final.show(truncate=False)
+
+
+
+
 
 if __name__ == "__main__":
 
